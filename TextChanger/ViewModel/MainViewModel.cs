a4eb54690c49d628cb30113f1f511bc4ad33e169
Priyanka Desai <Priyanka.Desai@Klingelnberg.com>
2022-07-04 11:59:06 +0530

    remove unnecessary usings

--- TextChanger/ViewModel/MainViewModel.cs
+++ TextChanger/ViewModel/MainViewModel.cs
@@ -1,14 +1,8 @@
 ﻿using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
 using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Media.Imaging;
 using TextChanger.Services;
 
 namespace TextChanger.ViewModel
